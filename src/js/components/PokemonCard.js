88aa6175aeec0f4dc64c2b475b669023c329d316
Himanshi-sa <himanshisahuhs@gmail.com>
2024-10-20 09:13:44 +0000

    final

---
+++ src/js/components/PokemonCard.js
@@ -0,0 +1,14 @@
+
+export function PokemonCard(image, name, description,link) {
+  const div = document.createElement("div");
+  div.classList.add("col");
+  div.innerHTML = `<div class="card">
+                <img src="${image}">
+                <div class="card-body">
+                  <h5 class="card-title">${name}</h5>
+                  <p class="card-text">${description}</p>
+                 <a class ="btn btn-warning" href ="${link}" target ="_blank">More info</a>
+                </div>
+              </div>`;
+  return div;
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,44 +1,51 @@
+import { shuffle } from "fast-shuffle";
+import Fuse from "fuse.js";
 import data from "./data.json";
-// console.log(data);
-// Dom Targeting
-const cardsRow = document.querySelector("#cards-row");
+import { PokemonCard } from "./components/PokemonCard";
+
+// DOM TARGETING
 const inputEl = document.querySelector("input");
-// add para data
-// const paragraph = document.createElement("p");
-// paragraph.textContent ="our class";
-// cardsRow.appendChild(paragraph);
-for (let pokemonobj of data ) {
-    const div = document.createElement("div");
-   div.classList.add("col");
-   div.innerHTML= `<div class="card">
-                <img src="${pokemonobj.image}">
-                <div class="card-body">
-                  <h5 class="card-title">${pokemonobj.name}</h5>
-                  <p class="card-text">${pokemonobj.description}</p>
-                </div>
-              </div>`;
-              cardsRow.appendChild(div);
+const cardsRowEl = document.querySelector("#cards-row");
+
+function renderPokemon(list) {
+  cardsRowEl.innerHTML = "";
+
+  for (let pokeObj of list) {
+    const pokemon = PokemonCard(
+      pokeObj.image,
+      pokeObj.name,
+      pokeObj.description,
+      pokeObj.link
+    );
+    cardsRowEl.appendChild(pokemon);
+  }
 }
 
-console.log(cardsRow);
-for (let pokemonobj of data) {
-    console.log(pokemonobj.name);
+function renderFilteredPokemon(term) {
+
+  const fuse = new Fuse(data,{
+    keys:["name"],
+  });
+   const filtered = fuse.search(term).map((obj) => obj.item);
+//   const filtered = [];
+//   for (let obj of data) {
+//     if (obj.name.toLowerCase().includes(term)) filtered.push(obj);
+//   }
+
+   renderPokemon(filtered);
 }
 
+// Input element on change
+inputEl.addEventListener("input", (event) => {
+  const currValue = event.target.value.toLowerCase().trim();
+  renderFilteredPokemon(currValue);
+});
 
-// console.log(inputEl)
-// inputEl.focus();
-// document.addEventListener("keypress",function (){
-// console.log("hello");
-// });
-// document.addEventListener("keypress",function (event){
-//     console.log(eventvent.key);
-// });
-// Focus input
-document.addEventListener("keyup",function (event){
-        if(event.key=== "/") {
-            // don't input
-            // event.preventDefault();
-            inputEl.focus();
-        };
-     });
+// Focus input on slash keypress
+document.addEventListener("keyup", (event) => {
+  if (event.key === "/") {
+    inputEl.focus();
+  }
+});
+
+renderPokemon(shuffle(data));
